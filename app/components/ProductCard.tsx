cb5e3db3fdbdecf98fa13e0ff5a48bd71d2dde75
Manoj <manoj.gupta16539@sakec.ac.in>
2024-08-31 14:14:07 +0530

    feat: Add AddToCart component and ProductCard component
    The code changes include adding two new components, `AddToCart` and `ProductCard`, to the application. The `AddToCart` component represents a button to add an item to the cart, while the `ProductCard` component displays a product card with the `AddToCart` button. These components are essential for the shopping functionality of the application.
    
    Note: The commit message format follows the convention of starting with a type (feat for feature) and providing a concise description of the changes.

---
+++ app/components/ProductCard.tsx
@@ -0,0 +1,13 @@
+'use client';
+import React from 'react'
+import AddToCart from './AddToCart';
+
+const ProductCard = () => {
+  return (
+    <div>
+        <AddToCart/>
+    </div>
+  )
+}
+
+export default ProductCard
