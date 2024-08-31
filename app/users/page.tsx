571265c753639951361439cb07350ce2619d725c
Manoj <manoj.gupta16539@sakec.ac.in>
2024-08-31 15:07:54 +0530

    Server side Rendering

--- app/users/page.tsx
+++ app/users/page.tsx
@@ -12,6 +12,8 @@ const UsersPage = async () => {
   return (
     <>
         <h1>Users</h1>
+        <p>{new Date().toLocaleDateString()}</p>
+        <p>{new Date().toLocaleTimeString()}</p>
         <ul>
             {users.map(user => <li key={user.id}>{user.name}</li>)}
             </ul>
