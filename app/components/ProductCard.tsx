33526ab3896f5bd98800e336b2204360c9d91b21
Manoj <manoj.gupta16539@sakec.ac.in>
2024-08-31 15:27:31 +0530

    CSS Modules

--- app/components/ProductCard.tsx
+++ app/components/ProductCard.tsx
@@ -1,10 +1,11 @@
 'use client';
 import React from 'react'
 import AddToCart from './AddToCart';
+import styles from './ProductCard.module.css';
 
 const ProductCard = () => {
   return (
-    <div>
+    <div className={styles.card}>
         <AddToCart/>
     </div>
   )
