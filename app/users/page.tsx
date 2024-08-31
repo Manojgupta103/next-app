58fd2224c68611427affc96cf8591c15ed717e7b
Manoj <manoj.gupta16539@sakec.ac.in>
2024-08-31 14:53:41 +0530

    Data Caching

--- app/users/page.tsx
+++ app/users/page.tsx
@@ -5,7 +5,8 @@ interface User {
 }
 
 const UsersPage = async () => {
-    const res =await fetch('https://jsonplaceholder.typicode.com/users');
+    const res =await fetch('https://jsonplaceholder.typicode.com/users',
+    {next: {revalidate: 10}} );
     const users: User[] = await res.json();
 
   return (
