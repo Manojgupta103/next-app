d91e74690c5acbf58ebaad04e933cb4b73c7f607
Manoj <manoj.gupta16539@sakec.ac.in>
2024-08-31 14:15:10 +0530

    Revert "feat: Add AddToCart component and ProductCard component"
    This reverts commit 5cf79411c4b2e0213425fcb69ccba7bcf24a7738.

--- app/page.tsx
+++ app/page.tsx
@@ -1,13 +1,7 @@
 import Image from "next/image";
-import Link from "next/link";
-import ProductCard from "./components/ProductCard";
 
 export default function Home() {
   return (
-    <main>
-      <h1>Hello World</h1>
-      <Link href="/users">Users</Link>
-      <ProductCard/>
-      </main>
+    <main><h1>Hello World</h1></main>
   );
 }
